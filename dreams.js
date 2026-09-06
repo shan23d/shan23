d8d4e3deb25bcb517e2a2b333d52df573762cbe7
shan23d <shantwothree23@gmail.com>
2026-09-07 06:19:41 +0630

    Add DREAMS object with meanings and relations

---
+++ dreams.js
@@ -0,0 +1,78 @@
+const DREAMS = {
+
+  "000": {
+    meaning: "သံဗူး",
+    related: "498"
+  },
+
+  "001": {
+    meaning: "နဂါး",
+    related: "545"
+  },
+
+  "002": {
+    meaning: "မှန်ဘီလူး",
+    related: "503"
+  },
+
+  "003": {
+    meaning: "ဓါးဆောင်သူ",
+    related: "502"
+  },
+
+  "023": {
+    meaning: "ဝက်",
+    related: "534"
+  },
+
+  "051": {
+    meaning: "ပုတီးစိပ်သူ",
+    related: "595"
+  },
+
+  "075": {
+    meaning: "ခွေးဖြူ",
+    related: "585"
+  },
+
+  "077": {
+    meaning: "ကြောင်",
+    related: "561"
+  },
+
+  "100": {
+    meaning: "ကြေငြာသူ",
+    related: "598"
+  },
+
+  "101": {
+    meaning: "ကောင်းကင်",
+    related: "645"
+  },
+
+  "145": {
+    meaning: "စုတ်တံ",
+    related: "601"
+  },
+
+  "150": {
+    meaning: "ကျားကွက်",
+    related: "648"
+  },
+
+  "601": {
+    meaning: "ရသေ့ဒေါက်ချာ",
+    related: "145"
+  },
+
+  "651": {
+    meaning: "ရေကူးကန်",
+    related: "145"
+  },
+
+  "999": {
+    meaning: "ပန်းကန်စင်",
+    related: "480"
+  }
+
+};
